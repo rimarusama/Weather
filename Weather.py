2fe9458472580030e49ec5cc0492473d8acaf936
Sudam Majhi <111006581+sudammajhi@users.noreply.github.com>
2023-01-06 23:40:17 +0530

    Update Weather.py
    Removed private pieces.

--- Weather.py
+++ Weather.py
@@ -5,7 +5,7 @@ s = HTMLSession()
 query = 'Delhi'
 url = f'https://www.google.com/search?q=weather+{query}'
 
-r = s.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'})
+r = s.get(url, headers={'User-Agent': ''})
 
 temp = r.html.find('span#wob_tm', first=True).text
 
